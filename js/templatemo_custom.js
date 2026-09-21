0d0f740f24bd0e37d8116700dc0e221e17ec6388
RakaSutji <109159524+RustOsprey55681@users.noreply.github.com>
2026-09-21 11:32:29 +0700

    URL Fragment and major fixes
    Each page now using URL Fragment or Anchor now, instead of shit idk whats the name. Also several fixes.

--- js/templatemo_custom.js
+++ js/templatemo_custom.js
@@ -2,58 +2,37 @@
 
 jQuery(document).ready(function($){
 
-	/************** Menu Content Opening *********************/
-	$(".main_menu a, .responsive_menu a").click(function(){
-		var id =  $(this).attr('class');
-		id = id.split('-');
+	/************** Fragment-based Page Navigation *********************/
+	var pages = {
+		home: { selector: ".homepage", link: ".templatemo_home" },
+		tools: { selector: ".service", link: ".templatemo_page2" },
+		gallery: { selector: ".portfolio", link: ".templatemo_page3" },
+		about: { selector: ".contact", link: ".templatemo_page5" }
+	};
+
+	function showPage(fragment) {
+		var page = pages[fragment] || pages.home;
+
 		$("#menu-container .content").hide();
-		$("#menu-container #menu-"+id[1]).addClass("animated fadeInDown").show();
-		$("#menu-container .homepage").hide();
-		$(".support").hide();
-		$(".testimonials").hide();
-		return false;
-	});
+		$(page.selector).addClass("animated fadeInDown").show();
+		$(".main_menu a, .responsive_menu a").removeClass("active");
+		$(".main_menu a" + page.link + ", .responsive_menu a" + page.link).addClass("active");
+	}
 
-	$( window ).load(function() {
-	  $("#menu-container .products").hide();
-	});
+	function showFragmentPage() {
+		showPage(window.location.hash.substring(1).toLowerCase());
+	}
 
-	$(".main_menu a.templatemo_home").addClass('active');
+	$(window).on("hashchange", showFragmentPage);
+	showFragmentPage();
 
-	$(".main_menu a.templatemo_home, .responsive_menu a.templatemo_home").click(function(){
-		$("#menu-container .homepage").addClass("animated fadeInDown").show();
-		$(this).addClass('active');
-		$(".main_menu a.templatemo_page2, .responsive_menu a.templatemo_page2").removeClass('active');
-		$(".main_menu a.templatemo_page3, .responsive_menu a.templatemo_page3").removeClass('active');
-		$(".main_menu a.templatemo_page5, .responsive_menu a.templatemo_page5").removeClass('active');
-		return false;
-	});
-
-	$(".main_menu a.templatemo_page2, .responsive_menu a.templatemo_page2").click(function(){
-		$("#menu-container .service").addClass("animated fadeInDown").show();
-		$(this).addClass('active');
-		$(".main_menu a.templatemo_home, .responsive_menu a.templatemo_home").removeClass('active');
-		$(".main_menu a.templatemo_page3, .responsive_menu a.templatemo_page3").removeClass('active');
-		$(".main_menu a.templatemo_page5, .responsive_menu a.templatemo_page5").removeClass('active');
-		return false;
-	});
-
-	$(".main_menu a.templatemo_page3, .responsive_menu a.templatemo_page3").click(function(){
-		$("#menu-container .portfolio").addClass("animated fadeInDown").show();
-		$(".our-services").show();
-		$(this).addClass('active');
-		$(".main_menu a.templatemo_page2, .responsive_menu a.templatemo_page2").removeClass('active');
-		$(".main_menu a.templatemo_home, .responsive_menu a.templatemo_home").removeClass('active');
-		$(".main_menu a.templatemo_page5, .responsive_menu a.templatemo_page5").removeClass('active');
-		return false;
-	});
-
-	$(".main_menu a.templatemo_page5, .responsive_menu a.templatemo_page5").click(function(){
-		$("#menu-container .contact").addClass("animated fadeInDown").show();
-		$(this).addClass('active');
-		$(".main_menu a.templatemo_page2, .responsive_menu a.templatemo_page2").removeClass('active');
-		$(".main_menu a.templatemo_page3, .responsive_menu a.templatemo_page3").removeClass('active');
-		$(".main_menu a.templatemo_home, .responsive_menu a.templatemo_home").removeClass('active');
+	$(".main_menu a[href^='#'], .responsive_menu a[href^='#']").click(function() {
+		var fragment = $(this).attr("href").substring(1).toLowerCase();
+		if (window.location.hash.substring(1).toLowerCase() !== fragment) {
+			window.location.hash = fragment;
+		} else {
+			showPage(fragment);
+		}
 		return false;
 	});
 
@@ -91,6 +70,8 @@ jQuery(document).ready(function($){
 
 
 /*----------------- Previous Next button ---------------------- */
+var pageNames = ["home", "tools", "gallery", "about"];
+
 $(window).load(function(){
 $(document).ready(function(){				   
     $(".divs div.content").each(function(e) {
@@ -99,24 +80,18 @@ $(document).ready(function(){
     });
     
 	$("#next").click(function(){
-        if ($(".divs div.content:visible").next().length != 0)
-            $(".divs div.content:visible").next().addClass("animated fadeInDown").show().prev().hide() ;
-        else {
-            $(".divs div.content:visible").hide();
-            $(".divs div.content:first").addClass("animated fadeInDown").show() ;
-			
-        }
+		var current = pageNames.indexOf(window.location.hash.substring(1).toLowerCase());
+		if (current < 0) current = 0;
+		var next = pageNames[(current + 1) % pageNames.length];
+		window.location.hash = next;
         return false;
     });
 
 	$("#prev").click(function(){
-        if ($(".divs div.content:visible").prev().length != 0)
-            $(".divs div.content:visible").prev().addClass("animated fadeInDown").show().next().hide();
-        else {
-            $(".divs div.content:visible").hide();
-            $(".divs div.content:last").addClass("animated fadeInDown").show();
-			
-        }
+		var current = pageNames.indexOf(window.location.hash.substring(1).toLowerCase());
+		if (current < 0) current = 0;
+		var previous = pageNames[(current - 1 + pageNames.length) % pageNames.length];
+		window.location.hash = previous;
         return false;
     });
 });
